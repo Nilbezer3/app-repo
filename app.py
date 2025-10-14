6774e1b310ec8e2fcb9d41ba0dc9f9e2d6f70384
Nilbezer3 <nilbezer09@icloud.com>
2025-10-14 09:55:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
 @app.get("/health")
 def health():
     return {"status": "ok"}
 from fastapi import FastAPI
 
-app = FastAPI()
 
 @app.get("/ping")
 def ping():
